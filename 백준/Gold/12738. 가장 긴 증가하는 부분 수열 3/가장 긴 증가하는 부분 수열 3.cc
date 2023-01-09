f22e9b21ef665fd2daeacae8b372f1955a0de5e9
Blackstar <85343553+YongJ7@users.noreply.github.com>
2023-01-10 00:49:12 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 196 ms, Memory: 8292 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N;
+vector<int> arr;
+
+int main()
+{
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    //freopen("input.txt","r",stdin);
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        int num;
+        cin >> num;
+        if(i==0) arr.push_back(num);
+        if (num > arr.back())
+        {
+            arr.push_back(num);
+        }
+        else
+        {
+            int idx = lower_bound(arr.begin(), arr.end(), num) - arr.begin();
+            arr[idx] = num;
+        }
+    }
+    cout << arr.size();
+}
