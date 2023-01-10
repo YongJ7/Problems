fcedbacd85c674f84324d182dd5712cf5388fd6a
Blackstar <85343553+YongJ7@users.noreply.github.com>
2023-01-10 20:16:59 +0900

    [level 1] Title: 행렬의 덧셈, Time: 55.06 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+    vector<vector<int>> answer;
+    for(int i=0;i<arr1.size();i++){
+        vector<int> row;
+        for(int j=0;j<arr1[i].size();j++){
+            row.push_back(arr1[i][j]+arr2[i][j]);
+        }
+        answer.push_back(row);
+    }
+    return answer;
+}
