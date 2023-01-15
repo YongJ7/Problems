651d6467a20d9e755a8fae63db0f3df0e3f042a5
Blackstar <85343553+YongJ7@users.noreply.github.com>
2023-01-15 17:08:56 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    cout << "|\\_/|" << '\n';
+    cout << "|q p|   /}" << '\n';
+    cout << "( 0 )\"\"\"\\" << '\n';
+    cout << "|\"^\"`    |" << '\n';
+    cout << "||_/=\\\\__|";
+}
