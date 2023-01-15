adbd53efaa79ffd2c926f99d992cdfa32f1517cc
Blackstar <85343553+YongJ7@users.noreply.github.com>
2023-01-15 17:11:34 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    if (x > 0 && y > 0)
+        cout << '1';
+    if (x < 0 && y > 0)
+        cout << '2';
+    if (x < 0 && y < 0)
+        cout << '3';
+    if (x > 0 && y < 0)
+        cout << '4';
+}
