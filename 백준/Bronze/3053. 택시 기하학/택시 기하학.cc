2b05bd26e3d07e7eef464131eed9137a3d911fa1
Blackstar <85343553+YongJ7@users.noreply.github.com>
2023-01-15 17:07:51 +0900

    [Bronze III] Title: 택시 기하학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define PI 3.141592653589
+using namespace std;
+int R;
+int main()
+{
+    cin >> R;
+    double u = (double)PI * R * R;
+    double t = (double)(2 * R) * R;
+    printf("%.6f\n", u);
+    printf("%.6f", t);
+}
